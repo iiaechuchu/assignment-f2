57f4befa193596f34a7d2ac564a60808d621be31
iiaechuchu <ii-aechutima@hotmail.com>
2016-04-20 01:41:09 +0700

    Added files via upload
    #01 สร้างหน้าต่างโปรแกรมกับสร้างยานอวกาศ

---
+++ GameEngine.java
@@ -0,0 +1,84 @@
+package Assignment.spw;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.geom.Rectangle2D;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import javax.swing.Timer;
+
+
+public class GameEngine implements KeyListener {
+	
+	GamePanel gp;
+		
+	private SpaceShip v;	
+	
+	private Timer timer;
+	
+	
+	public GameEngine(GamePanel gp, SpaceShip v) {
+		this.gp = gp;
+		this.v = v;		
+		
+		gp.sprites.add(v);
+		
+		timer = new Timer(50, new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				process();
+			}
+		});
+		timer.setRepeats(true);
+		
+	}
+	
+	public void start(){
+		timer.start();
+	}
+	
+
+	
+	private void process(){
+
+		gp.updateGameUI();
+		
+		Rectangle2D.Double vr = v.getRectangle();
+
+	}
+	
+	
+	void controlVehicle(KeyEvent e) {
+		switch (e.getKeyCode()) {
+		case KeyEvent.VK_LEFT:
+			v.move(-1);
+			break;
+		case KeyEvent.VK_RIGHT:
+			v.move(1);
+			break;
+		case KeyEvent.VK_D:
+			//difficulty += 0.1;
+			break;
+		}
+	}
+
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		controlVehicle(e);
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		//do nothing
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		//do nothing		
+	}
+}
